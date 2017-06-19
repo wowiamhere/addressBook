40e105aafa97a65f484dbe315076caa2f42b7a64
wowiamhere <sergiognrl@hotmail.com>
2017-06-19 16:29:34 -0700

    refractore db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,29 @@
-@user = User.create email: "user1@email.com", 
-  password: "password", 
-  password_confirmation: "password"
+@user1 = User.create email: 'user1@email.com', 
+  password: 'password', 
+  password_confirmation: 'password'
+
+@user2 = User.create email: 'user2@email.com',
+  password: 'password',
+  password_confirmation: 'password'
 
 20.times do |each|
   Contact.create(
-    name: "contact_#{each}",
-    email: "contact_#{each}@email.com",
-    phone: "1800 123 1234",
-    address: "123 st. Here, State 12342",
-    company: "contact_#{each} company",
-    birtday: DateTime.strptime("1/29/1980", "%m/%d/%Y" ),
-    user_id: @user.id)
+    name: 'contact_#{each}',
+    email: 'contact_#{each}@email.com',
+    phone: '1800 123 1234',
+    address: '123 st. Here, State 12342',
+    company: 'contact_#{each} company',
+    birtday: DateTime.strptime('1/29/1980', '%m/%d/%Y' ),
+    user_id: @user1.id)
+end
+
+20.times do |each|
+  Contact.create(
+    name: 'contact_#{each}',
+    email: 'contact_#{each}@email.com',
+    phone: '1800 123 1234',
+    address: '123 st. Here, State 12342',
+    company: 'contact_#{each} company',
+    birtday: DateTime.strptime('1/29/1980', '%m/%d/%Y' ),
+    user_id: @user2.id)
 end
